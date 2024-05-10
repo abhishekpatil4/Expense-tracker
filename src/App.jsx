927f05cdffea638d36f5b194bf77f02258f6faff
Abhishek Patil <abishkpatil@gmail.com>
2024-05-10 18:15:20 +0530

    feat: add amountCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import { Navbar } from './components/navbar/Navbar'
+import { AmountCard } from './components/amountCard/AmountCard'
 
 function App() {
   return (
-    <>
+    <div style={{margin:'1rem'}}>
       <Navbar />
-    </>
+      <div className="wrapper">
+        <AmountCard type={"balance"} amountValue={"5000"} />
+        <AmountCard type={"expense"} amountValue={"500"} />
+        <AmountCard type={"expense"} amountValue={"500"} />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/amountCard/AmountCard.jsx
@@ -0,0 +1,19 @@
+import "./styles.css"
+
+export const AmountCard = ({ type, amountValue }) => {
+    return <>
+        {
+            type === "balance" ?
+                <div className="card-wrapper">
+                    <div>Wallet Balance: ₹{amountValue}</div>
+                    <button id="add-income">+ Add Income</button>
+                </div>
+                :
+                <div className="card-wrapper">
+                    <div>Expenses: ₹{amountValue}</div>
+                    <button id="add-income">+ Add Expense</button>
+                </div>
+
+        }
+    </>
+}
