24fd49e1fea5db09753845b36066848bee815686
Abhishek Patil <abishkpatil@gmail.com>
2024-05-10 19:16:55 +0530

    feat: add piechart

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import { Navbar } from './components/navbar/Navbar'
 import { AmountCard } from './components/amountCard/AmountCard'
+import { ExpPiechart } from './components/expPiechart/ExpPiechart'
 
 function App() {
   return (
-    <div style={{margin:'1rem'}}>
+    <div style={{ margin: '1rem' }}>
       <Navbar />
       <div className="wrapper">
         <AmountCard type={"balance"} amountValue={"5000"} />
         <AmountCard type={"expense"} amountValue={"500"} />
-        <AmountCard type={"expense"} amountValue={"500"} />
+        <ExpPiechart />
       </div>
     </div>
   )
