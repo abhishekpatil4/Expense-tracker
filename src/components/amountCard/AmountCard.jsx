b5656fce251fda5a3e8e1a8ce9b2d7cb3e0a8b10
Abhishek Patil <abishkpatil@gmail.com>
2024-05-11 16:17:14 +0530

    fix: minor bugs

--- src/components/amountCard/AmountCard.jsx
+++ src/components/amountCard/AmountCard.jsx
@@ -3,11 +3,19 @@ import { useState, useEffect } from "react"
 import ReactModal from "react-modal";
 
 export const AmountCard = ({ type }) => {
-    const [modelOpen, setModelOpen] = useState(true);
+    const [modelOpen, setModelOpen] = useState(false);
+    const [newBalance, setNewBalance] = useState();
     const [amount, setAmount] = useState({
         currentBalance: 0,
         currentExpenses: 0
     });
+    const handleAddBalance = () => {
+        const currentAmount = JSON.parse(localStorage.getItem("currentAmount"));
+        currentAmount.currentBalance = Number(currentAmount.currentBalance) +  Number(newBalance);
+        localStorage.setItem("currentAmount", JSON.stringify(currentAmount));
+        setAmount(JSON.parse(localStorage.getItem("currentAmount")));
+        setModelOpen(false)
+    }
     useEffect(() => {
         setAmount(JSON.parse(localStorage.getItem("currentAmount")));
     }, [])
@@ -23,41 +31,48 @@ export const AmountCard = ({ type }) => {
                             margin: 'auto',
                             maxWidth: '520px',
                             maxHeight: '150px',
-                            backgroundColor:'#EEEEEE',
-                            borderRadius:'12px',
-                            border:'0px',
-                            padding:'1.5rem 2rem'
+                            backgroundColor: '#EEEEEE',
+                            borderRadius: '12px',
+                            border: '0px',
+                            padding: '1.8rem 2rem'
                         }
                     }}>
-                        <div style={{display:'flex', flexDirection:'column', gap:'0.5rem'}}>
+                        <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
                             <h1>Add Balance</h1>
-                            <div style={{display:'flex', justifyContent:'space-between'}}>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', flexDirection:'row'}}>
                                 <input type="text" placeholder="Income Amount" style={{
-                                    borderRadius:'10px',
-                                    border:'0px',
-                                    boxShadow:'0px 3px #C1C1C1C1',
-                                    width:'15rem',
-                                    fontSize:'1.2rem',
-                                    padding:'0.8rem',
-                                }}/>
-                                <button style={{
-                                    backgroundColor: '#F4BB4A',
-                                    border:'0px',
-                                    borderRadius:'10px',
-                                    color:'white',
-                                    boxShadow:'0px 3px #C1C1C1C1',
-                                    fontSize:'1.2rem',
-                                    padding:'0.8rem'
-                                }}>Add Balance</button>
-                                <button style={{
-                                    backgroundColor: '#E2E2E2',
-                                    border:'0px',
-                                    borderRadius:'10px',
-                                    color:'black',
-                                    boxShadow:'0px 3px #C1C1C1C1',
-                                    fontSize:'1.2rem',
-                                    padding:'0.8rem'
-                                }} onClick={() => setModelOpen(false)}>Cancel</button>
+                                    borderRadius: '10px',
+                                    border: '0px',
+                                    boxShadow: '0px 3px #C1C1C1C1',
+                                    width: '15rem',
+                                    fontSize: '1.2rem',
+                                    padding: '0.8rem',
+                                }} 
+                                value={newBalance}
+                                onChange={(e) => setNewBalance(e.target.value)}
+                                />
+                                <div style={{display:'flex', justifyContent:'space-around', minWidth:'240px'}}>
+                                    <button style={{
+                                        backgroundColor: '#F4BB4A',
+                                        border: '0px',
+                                        borderRadius: '10px',
+                                        color: 'white',
+                                        boxShadow: '0px 3px #C1C1C1C1',
+                                        fontSize: '1.2rem',
+                                        padding: '0.8rem'
+                                    }} 
+                                    onClick={handleAddBalance}
+                                    >Add Balance</button>
+                                    <button style={{
+                                        backgroundColor: '#E2E2E2',
+                                        border: '0px',
+                                        borderRadius: '10px',
+                                        color: 'black',
+                                        boxShadow: '0px 3px #C1C1C1C1',
+                                        fontSize: '1.2rem',
+                                        padding: '0.8rem'
+                                    }} onClick={() => setModelOpen(false)}>Cancel</button>
+                                </div>
                             </div>
                         </div>
                     </ReactModal>
