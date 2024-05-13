6c7fb38691fa81e7c702a96f0524d9aac10b9477
Abhishek Patil <abishkpatil@gmail.com>
2024-05-13 22:45:58 +0530

    feat: add dynamic recent transactions

--- src/components/recentTransactions/RecentTransaction.jsx
+++ src/components/recentTransactions/RecentTransaction.jsx
@@ -1,6 +1,7 @@
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { Transaction } from "./Transaction";
+import { useState } from "react";
 
 const data = [
     {
@@ -10,9 +11,13 @@ const data = [
 ]
 
 export const RecentTransaction = () => {
+    const [transactionData, setTransactionData] = useState();
+    useState(() => {
+        setTransactionData(JSON.parse(localStorage.getItem("transactions")));
+    }, [])
     return <>
         <div>
-            <h1 style={{ color: 'white' }}><i>Recent Transactions</i></h1>
+            {/* <h1 style={{ color: 'white' }}><i>Recent Transactions</i></h1>
             <div style={{ backgroundColor: 'white', borderRadius: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
                 <Transaction name={"Pizza"} dateTime={"March 20, 2024"} price={200} />
                 <Transaction name={"Pizza"} dateTime={"March 20, 2024"} price={200} />
@@ -21,6 +26,21 @@ export const RecentTransaction = () => {
                     <button style={{borderRadius:'15px', padding:'14px', border:'0px', boxShadow:'0px 2px #C3C3C3', display:'flex', color:'black'}}><FaArrowLeftLong /></button>
                     <span style={{borderRadius:'10px', padding:'10px 15px', border:'0px', boxShadow:'0px 2px #C3C3C3', backgroundColor:'#43967B', color:'white', display:'flex'}}>1</span>
                     <button style={{borderRadius:'15px', padding:'14px', border:'0px', boxShadow:'0px 2px #C3C3C3', display:'flex', color:'black'}}><FaArrowRightLong /></button>
+                </div>
+            </div> */}
+            <h1 style={{ color: 'white' }}><i>Recent Transactions</i></h1>
+            <div style={{ backgroundColor: 'white', borderRadius: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                {transactionData &&
+                    transactionData.slice(2,5).map((transactionData, idx) => 
+                        <Transaction key={idx} name={transactionData.title} dateTime={transactionData.dateTime} price={transactionData.price} />
+                    )
+                }
+                {/* <Transaction name={"Pizza"} dateTime={"March 20, 2024"} price={200} />
+                <Transaction name={"Pizza"} dateTime={"March 20, 2024"} price={200} /> */}
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 20, padding: '0.8rem' }}>
+                    <button style={{ borderRadius: '15px', padding: '14px', border: '0px', boxShadow: '0px 2px #C3C3C3', display: 'flex', color: 'black' }}><FaArrowLeftLong /></button>
+                    <span style={{ borderRadius: '10px', padding: '10px 15px', border: '0px', boxShadow: '0px 2px #C3C3C3', backgroundColor: '#43967B', color: 'white', display: 'flex' }}>1</span>
+                    <button style={{ borderRadius: '15px', padding: '14px', border: '0px', boxShadow: '0px 2px #C3C3C3', display: 'flex', color: 'black' }}><FaArrowRightLong /></button>
                 </div>
             </div>
         </div>
