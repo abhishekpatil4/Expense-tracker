cd73a85baf703ca2ca6ca49dc54aeb5ed74217c3
Abhishek Patil <abishkpatil@gmail.com>
2024-05-13 23:04:10 +0530

    fix: code refactor

--- src/components/topexpenses/TopExpensesBarChart.jsx
+++ src/components/topexpenses/TopExpensesBarChart.jsx
@@ -1,39 +1,6 @@
 import React, { PureComponent } from 'react';
 import { BarChart, Bar, ResponsiveContainer, Legend, XAxis, YAxis, Tooltip } from 'recharts';
 
-const data = [
-    {
-        name: 'Page A',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: 'Page B',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: 'Page C',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
-    },
-    {
-        name: 'Page D',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
-    },
-    {
-        name: 'Page E',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-    },
-];
-
 export const TopExpensesBarChart = () => {
     return <>
         <ResponsiveContainer width={'99%'} height={272}>
