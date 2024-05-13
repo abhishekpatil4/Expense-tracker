7a54c1d70777396ffd46370c2ba44f92a65a6180
Abhishek Patil <abishkpatil@gmail.com>
2024-05-13 19:25:27 +0530

    feat: add add expense

--- src/components/amountCard/AmountCard.jsx
+++ src/components/amountCard/AmountCard.jsx
@@ -6,10 +6,23 @@ export const AmountCard = ({ type }) => {
     const [balanceModelOpen, setBalanceModelOpen] = useState(false);
     const [expenseModelOpen, setExpenseModelOpen] = useState(false);
     const [newBalance, setNewBalance] = useState();
+
+    const [title, setTitle] = useState();
+    const [price, setPrice] = useState();
+    const [category, setCategory] = useState();
+    const [dateTime, setDateTime] = useState();
+
     const [amount, setAmount] = useState({
         currentBalance: 0,
         currentExpenses: 0
     });
+    const handleAddExpense = () => {
+        console.log(title);
+        console.log(price);
+        console.log(category);
+        console.log(dateTime);
+        setExpenseModelOpen(false)
+    }
     const handleAddBalance = () => {
         const currentAmount = JSON.parse(localStorage.getItem("currentAmount"));
         if (newBalance == "") {
@@ -92,8 +105,8 @@ export const AmountCard = ({ type }) => {
                             justifyContent: 'center',
                             flexDirection: 'column',
                             margin: 'auto',
-                            maxWidth: '520px',
-                            maxHeight: '150px',
+                            maxWidth: '500px',
+                            maxHeight: '300px',
                             backgroundColor: '#EEEEEE',
                             borderRadius: '12px',
                             border: '0px',
@@ -102,18 +115,66 @@ export const AmountCard = ({ type }) => {
                     }}>
                         <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
                             <h1>Add Balance</h1>
-                            <div style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row' }}>
-                                <input type="text" placeholder="Income Amount" style={{
-                                    borderRadius: '10px',
-                                    border: '0px',
-                                    boxShadow: '0px 3px #C1C1C1C1',
-                                    width: '15rem',
-                                    fontSize: '1.2rem',
-                                    padding: '0.8rem',
-                                }}
-                                    value={newBalance}
-                                    onChange={(e) => setExpenseModelOpen(e.target.value)}
-                                />
+                            <div style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                            }}>
+                                {/* (1/2) 2 flexbox for input elements */}
+                                <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                    <input type="text" placeholder="Title" style={{
+                                        borderRadius: '10px',
+                                        border: '0px',
+                                        boxShadow: '0px 3px #C1C1C1C1',
+                                        width: '12rem',
+                                        fontSize: '1.2rem',
+                                        padding: '0.8rem',
+                                        margin: "0.5rem 0.5rem"
+                                    }}
+                                        value={title}
+                                        onChange={(e) => setTitle(e.target.value)}
+                                    />
+                                    <input type="text" placeholder="Price" style={{
+                                        borderRadius: '10px',
+                                        border: '0px',
+                                        boxShadow: '0px 3px #C1C1C1C1',
+                                        width: '12rem',
+                                        fontSize: '1.2rem',
+                                        padding: '0.8rem',
+                                        margin: "0.5rem 0.5rem"
+                                    }}
+                                        value={price}
+                                        onChange={(e) => setPrice(e.target.value)}
+                                    />
+                                </div>
+                                {/* (2/2) 2 flexbox for input elements */}
+                                <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                    <input type="dropdown" placeholder="Select Category" style={{
+                                        borderRadius: '10px',
+                                        border: '0px',
+                                        boxShadow: '0px 3px #C1C1C1C1',
+                                        width: '12rem',
+                                        fontSize: '1.2rem',
+                                        padding: '0.8rem',
+                                        margin: "0.5rem 0.5rem"
+                                    }}
+                                        value={category}
+                                        onChange={(e) => setCategory(e.target.value)}
+                                    />
+                                    <input type="text" placeholder="dd/mm/yyyy" style={{
+                                        borderRadius: '10px',
+                                        border: '0px',
+                                        boxShadow: '0px 3px #C1C1C1C1',
+                                        width: '12rem',
+                                        fontSize: '1.2rem',
+                                        padding: '0.8rem',
+                                        margin: "0.5rem 0.5rem"
+                                    }}
+                                        value={dateTime}
+                                        onChange={(e) => setDateTime(e.target.value)}
+                                    />
+                                </div>
+
+                                {/* flex box for buttons */}
                                 <div style={{ display: 'flex', justifyContent: 'space-around', minWidth: '240px' }}>
                                     <button style={{
                                         backgroundColor: '#F4BB4A',
@@ -122,9 +183,10 @@ export const AmountCard = ({ type }) => {
                                         color: 'white',
                                         boxShadow: '0px 3px #C1C1C1C1',
                                         fontSize: '1.2rem',
-                                        padding: '0.8rem'
+                                        padding: '0.8rem',
+                                        margin: "0.5rem 0.5rem"
                                     }}
-                                        onClick={handleAddBalance}
+                                        onClick={handleAddExpense}
                                     >Add Balance</button>
                                     <button style={{
                                         backgroundColor: '#E2E2E2',
@@ -133,7 +195,8 @@ export const AmountCard = ({ type }) => {
                                         color: 'black',
                                         boxShadow: '0px 3px #C1C1C1C1',
                                         fontSize: '1.2rem',
-                                        padding: '0.8rem'
+                                        padding: '0.8rem',
+                                        margin: "0.5rem 0.5rem"
                                     }} onClick={() => setExpenseModelOpen(false)}>Cancel</button>
                                 </div>
                             </div>
